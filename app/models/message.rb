1240651a5776f3d58322b657b478724aef87856f
Lisakinomayoi <cynthianishimura0501@gmail.com>
2021-10-01 06:27:31 +0900

    画像投稿設定の間違え直し

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,7 +3,7 @@ class Message < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :content, presence: true #messageにしない
+  validates :content, presence: true, unless: :was_attached?  #messageにしない
 
   def was_attached?
     self.image.attached?
