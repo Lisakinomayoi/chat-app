6998296ca7d4fdbabe2e876dd3d49b1ccd9b52aa
Lisakinomayoi <cynthianishimura0501@gmail.com>
2021-10-01 01:46:45 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,8 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
-  end
+    @messages = @room.messages.includes(:user)#チャットルームに紐づいている全てのメッセージ（@room.messages)を@messagesと定義
+  end                                         #この場合メッセージとユーザー情報の取得で二倍のアクセスが必要になるのでN＋1問題発生。そこでincludes使用して一回のアクセスにまとめる。
   
   def create
     @room = Room.find(params[:room_id])
@@ -10,6 +11,7 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to room_messages_path(@room)
     else
+      @messages = @room.messages.includes(:user)
       render :index  #index.html.erb
     end  
   end
